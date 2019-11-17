f408091a5eecadd4dd247d165b533b8600454c50
Mads Opheim <mads.opheim@gmail.com>
2019-11-17 22:41:58 +0100

    Personalization

--- madsopheim.com/gridsome.config.js
+++ madsopheim.com/gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'Mads Opheim',
+  siteDescription: 'Tech lead. Java. TDD, DDD, Modern agile.',
 
   templates: {
     Post: '/:title',
