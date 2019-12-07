7dae187e3ad0ce5c2b73fd46fae4f631bb1e4d6d
Mads Opheim <mads.opheim@gmail.com>
2019-12-07 10:01:12 +0100

    Added re-routing of _pages/talks

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import '~/assets/style/index.scss'
 
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
+import Talks from '~/pages/talks.vue'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
   
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-}
+
+  router.addRoutes([
+    {
+      path: '/_pages/talks',
+      name: 'oldTalksPage',
+      component: Talks
+    }
+  ])
+};
