7dd051685787385bad638b7df79a275287979e6d
Mads Opheim <mads.opheim@gmail.com>
2026-08-29 11:56:29 +0200

    Fix config

--- app/[format]/route.ts
+++ app/[format]/route.ts
@@ -1,6 +1,6 @@
 import { Feed } from "feed";
-import { getBlogPosts } from "app/lib/posts";
-import { metaData } from "app/config";
+import { getBlogPosts } from "@/app/lib/posts";
+import { metaData } from "@/app/config";
 import { NextResponse } from "next/server";
 
 export async function generateStaticParams() {
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { formatDate, getBlogPosts } from "app/lib/posts";
+import { formatDate, getBlogPosts } from "@/app/lib/posts";
 
 export const metadata = {
   title: "Blog",
